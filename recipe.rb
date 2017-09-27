94fc07e0a8fbbafe79981d637c4c2c9cfcb652c4
otofu-square <yukita@feedforce.jp>
2017-09-27 15:51:04 +0900

    :package: Install nginx by mitamae

---
+++ recipe.rb
@@ -0,0 +1,12 @@
+# Get info for newest nginx
+# see: https://qiita.com/hiroq/items/420424bc500d89fd1cc8
+execute "get info for newest nginx" do
+  command 'wget http://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key && rm nginx_signing.key'
+  command 'VCNAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d= -f2` && sh -c "echo \"deb http://nginx.org/packages/ubuntu/ $VCNAME nginx\" >> /etc/apt/sources.list"'
+  command 'VCNAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d= -f2` && sh -c "echo \"deb-src http://nginx.org/packages/ubuntu/ $VCNAME nginx\" >> /etc/apt/sources.list"'
+  command 'apt-get update"'
+end
+
+package "nginx" do
+  action :install
+end
