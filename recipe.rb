bd159a22090f766dacff90d0ea7bfe75b713ad83
otofu-square <yukita@feedforce.jp>
2017-09-30 08:19:54 +0900

    :wrench: Fixed recipe.rb

--- recipe.rb
+++ recipe.rb
@@ -4,7 +4,7 @@ execute "get info for newest nginx" do
   command 'wget http://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key && rm nginx_signing.key'
   command 'VCNAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d= -f2` && sh -c "echo \"deb http://nginx.org/packages/ubuntu/ $VCNAME nginx\" >> /etc/apt/sources.list"'
   command 'VCNAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d= -f2` && sh -c "echo \"deb-src http://nginx.org/packages/ubuntu/ $VCNAME nginx\" >> /etc/apt/sources.list"'
-  command 'apt-get update"'
+  command 'apt-get update'
 end
 
 package "nginx" do
