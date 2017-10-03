1a5d00a0e2752ada00750ad002af45f05a549fcf
otofu-square <yukita@feedforce.jp>
2017-10-03 22:21:26 +0900

    :+1: Add database module

---
+++ database.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Todo struct {
+	gorm.Model
+	Title     string `json:"title"`
+	Completed int    `json:"completed"`
+}
+
+type TransformedTodo struct {
+	ID        uint   `json:"id"`
+	Title     string `json:"title"`
+	Completed int    `json:"completed"`
+}
+
+func Database() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:@demo?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
