a9f9d79229efe261aecddc64d3b3a6838a0901cc
otofu-square <yukita@feedforce.jp>
2017-10-03 17:23:15 +0900

    :+1: Add basic gin web app

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type RequestBody struct {
-}
-
 func main() {
-	token := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
-	endpoint := "https://api.line.me/v2/bot/message/push"
-
-	json.Marshal()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+	r.Run()
 }
